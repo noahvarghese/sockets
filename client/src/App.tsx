aded2da576c6d891e57142cfb126277354d5489d
noahvarghese <varghese.noah@gmail.com>
2020-10-18 14:48:15 -0400

    basic socket connections work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,32 @@
 import React from "react";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import Main from "./Main";
-import reducer from "./redux/reducer";
+import { useState, useEffect } from "react";
+// import { createStore } from "redux";
+// import { Provider } from "react-redux";
+// import Main from "./Main";
+// import reducer from "./redux/reducer";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:3000/";
 
-const store = createStore(reducer);
+// const store = createStore(reducer);
 
-const App = () => (
-	<Provider store={store}>
-		<Main />
-	</Provider>
-);
+const App = () => {
+	// <Provider store={store}>
+	// 	<Main />
+	// </Provider>
+	const [response, setResponse] = useState("");
+	// const socket = socketIOClient(ENDPOINT);
+	// // socket.on("FromAPI", (data) => {
+	// // 	setResponse(data);
+	// // });
+
+	useEffect(() => {
+		const socket = socketIOClient(ENDPOINT);
+
+		socket.on("FromAPI", (data) => {
+			setResponse(data);
+		});
+	}, []);
+	return <div>{response}</div>;
+};
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -7,31 +7,41 @@ import {
 } from "path";
 import http from "http";
 import socketIo from "socket.io";
+import cors from "cors";
 
 const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = dirname(__filename);
 
+const whiteList = [
+    "http://localhost:3001"
+];
+
 
 (async () => {
     const app = express();
+    app.use(cors({
+        origin: (origin, callback) => {
+            if (whiteList.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(null, false);
+            }
+        },
+        credentials: true
+    }))
     app.use(express.json());
     app.use(express.static(__dirname + "/public/"));
     const server = http.createServer(app);
     const io = socketIo(server);
 
-    app.get(/.*/, (_, res) => {
+    app.get("/", (_, res) => {
         res.sendFile(__dirname + "/public/index.html");
     });
 
-    app.listen(3000, () => {
-        console.log("Server is ready");
-    });
-
-    const getApiAndEmit = socket => {
-        const response = new Date();
-        socket.emit("FromAPI", response);
-    }
+    // app.listen(3000, () => {
+    //     console.log("Server is ready");
+    // });
 
     let interval;
     io.on("connection", socket => {
@@ -45,5 +55,15 @@ const __dirname = dirname(__filename);
             console.log("Client disconnected");
             clearInterval(interval);
         });
+    });
+
+    const getApiAndEmit = socket => {
+        const response = new Date();
+        console.log(response);
+        socket.emit("FromAPI", response);
+    }
+
+    server.listen(3000, () => {
+        console.log("listening on *:3000");
     })
 })();
